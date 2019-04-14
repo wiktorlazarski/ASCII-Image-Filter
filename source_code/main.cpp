96f500a1bbb22599ea8de92365d57ec92dde27fa
Wiktor Łazarski <wjlazarski@gmail.com>
2019-04-14 20:19:19 +0200

    Setting up OpenCV library

--- source_code/main.cpp
+++ source_code/main.cpp
@@ -1,6 +1,11 @@
-#include <iostream>
+#include <opencv2/opencv.hpp>
 
 int main(int argc, char* argv[])
 {
+	cv::Mat img = cv::imread(argv[1]);
+	cv::namedWindow("image", cv::WINDOW_NORMAL);
+	cv::imshow("image", img);
+	cv::waitKey(0);
 
+	return 0;
 }
